cf14ab678c833e50c2327c709d3dba23d65784f2
minsu.rob <minsu.rob@gmail.com>
2022-02-28 00:53:11 +0900

    (chapter 3-5) title, container, todoBlock css element 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from "react";
+import "./App.css"
+
 
 export default class App extends Component {
   render() {
-    return{
-      <div>
-        hello
+    return(
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할일</h1>
+          </div>
+        </div>
       </div>
-    }
-  }
+    )
+}
 }

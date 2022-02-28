3369d0f068287cca62581486ab4b55ddef40f630
minsuLee <minsu.rob@gmail.com>
2022-02-28 18:33:20 +0900

    (Chapter 4-6) input form 창을 컴포넌트 분리
    챕터4와 비슷한 작업이나, 사용자 구현 함수를 prop에 파라미터로 전달함

--- src/App.js
+++ src/App.js
@@ -3,20 +3,13 @@ import "./App.css"
 import {useState} from 'react'
 
 import List from './components/List'
+import Form from './components/Form'
 
 export default function App(){
   
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
 
-
-
-
- 
-  const handleChange=(e) => {
-    setValue(e.target.value);
-  };
-  
   const handleSubmit=(e) => {
     e.preventDefault();
 
@@ -38,26 +31,7 @@ export default function App(){
           </div>
 
           <List todoData={todoData} setTodoData={setTodoData}/>
-          
-
-
-        <form style={{display : "flex"}} onSubmit={handleSubmit}>
-          <input
-           type="text"
-           name="value"
-           style={{flex:"10", padding:"5px"}}
-           placeholoder="write the todo task"
-           value={value}
-           onChange={handleChange}
-          />
-          <input
-           type="submit"
-           value="입력"
-           className="btn"
-           style={{flex: '1'}}
-          />
-
-        </form>
+          <Form handleSubmit={handleSubmit} value={value} setValue={setValue}/>
 
         </div>
       </div>

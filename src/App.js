5ec661f700ea207bf0eb036ad17a3a8a949001a5
minsuLee <minsu.rob@gmail.com>
2022-02-28 17:22:55 +0900

    (Chapter 4-4) 컴포넌트를 따로 분리하여 import하는 형식으로 변경
    컴포넌트간 데이터 이동은 props을 통하여 파라미터는 넘겨서 사용.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css"
 import {useState} from 'react'
 
+import List from './components/List'
 
 export default function App(){
   
@@ -9,30 +10,9 @@ export default function App(){
   const [value, setValue] = useState("");
 
 
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right"
-  }
-
-  const getStyle= (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none"
-    }
-  }
 
 
-  const handleClick=(id) => {
-    let newTodoData = todoData.filter(data=> data.id !== id);
-    //console.log('newTd', newTodoData);
-    setTodoData(todoData)
-  }
-
+ 
   const handleChange=(e) => {
     setValue(e.target.value);
   };
@@ -50,16 +30,6 @@ export default function App(){
 
   }
 
-  const handleCompleteChange = (id) =>{
-    let newTodoData = todoData.map((data)=> {
-      if (data.id === id) {
-        // console.log('handleCompleteChange', id + ", "+ data.id);
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodoData(newTodoData);
-  }
     return(
       <div className="container">
         <div className="todoBlock">
@@ -67,13 +37,8 @@ export default function App(){
             <h1>할일</h1>
           </div>
 
-          {todoData.map((data)=>(
-          <div style={getStyle(data.completed)} key={data.id}>
-            <input type="checkbox" defaultChecked={false} onChange={()=> handleCompleteChange(data.id)}/>
-             {data.title}
-            <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
-          </div>
-          ))}
+          <List todoData={todoData} setTodoData={setTodoData}/>
+          
 
 
         <form style={{display : "flex"}} onSubmit={handleSubmit}>

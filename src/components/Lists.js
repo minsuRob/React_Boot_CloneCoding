6f4abc8e5c6b2f419486f7b4f837b74309aa29f0
minsuLee <minsu.rob@gmail.com>
2022-03-01 00:47:02 +0900

    (Chapter 4-11) List와 Lists 컴포넌트 세분화
    큰 범주에 있는 Lists는 Drag 관련 처리를 주로 진행하고
    List는 실제 데이터를 표현하는 것으로 기술을 분리

---
+++ src/components/Lists.js
@@ -0,0 +1,51 @@
+import { list } from 'postcss';
+import React from 'react'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+import List from './List'
+
+export default function Lists({todoData, setTodoData}) {
+    
+
+
+      const handleEnd = (result) => {
+        if (!result.destination) return;
+        const newTodoData = todoData;
+        const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+        newTodoData.splice(result.destination.index, 0, reorderedItem);
+        setTodoData(newTodoData);
+      }
+
+   return (
+    <div>
+       <DragDropContext onDragEnd={handleEnd}>
+         <Droppable droppableId="todo">
+           {(provided) =>(
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+            {todoData.map((data, index)=>(
+              <Draggable 
+              key={data.id}
+              draggableId={data.id.toString()} 
+              index={index}>
+
+            {(provided, snapshot)=>(
+              <List
+                key={data.id}
+                id={data.id}
+                title={data.title}
+                completed={data.completed}
+                todoData={todoData}
+                setTodoData={setTodoData}
+                provided={provided}
+                snapshot={snapshot}
+              />
+          )}
+        </Draggable>
+       ))}
+          </div>
+          )}
+          </Droppable>
+          </DragDropContext>
+    </div>
+  );
+}

3a35539701974c062fe7e9e1bf5d2df6aa81721d
minsuLee <minsu.rob@gmail.com>
2022-03-01 01:09:16 +0900

    refac : repair code indent.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,31 @@
 import React from 'react'
 
-export default function Form({handleSubmit, value, setValue}) {
-    const handleChange=(e) => {
-        setValue(e.target.value);
-    };
+export default function Form({ handleSubmit, value, setValue }) {
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
-    
-  
+
+
   return (
     <div>
-        <form onSubmit={handleSubmit}
-         className="flex pt-2"
-        >
-          <input
-           type="text"
-           name="value"
-           placeholoder="write the todo task"
-           value={value}
-           onChange={handleChange}
-           className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
-          />
-          <input
-           type="submit"
-           value="입력"
-           className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200"
-          />
-        </form>  
+      <form onSubmit={handleSubmit}
+        className="flex pt-2"
+      >
+        <input
+          type="text"
+          name="value"
+          placeholoder="write the todo task"
+          value={value}
+          onChange={handleChange}
+          className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
+        />
+        <input
+          type="submit"
+          value="입력"
+          className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200"
+        />
+      </form>
     </div>
   )
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,39 +1,39 @@
 import React from 'react'
 
-const List = 
-({id, title, completed, todoData, setTodoData, provided, snapshot, }) => {
-        const handleClick=(id) => {
-            let newTodoData = todoData.filter(data=> data.id !== id);
+const List =
+    ({ id, title, completed, todoData, setTodoData, provided, snapshot, }) => {
+        const handleClick = (id) => {
+            let newTodoData = todoData.filter(data => data.id !== id);
             //console.log('newTd', newTodoData);
             setTodoData(todoData)
         }
 
-        const handleCompleteChange = (id) =>{
-            let newTodoData = todoData.map((data)=> {
+        const handleCompleteChange = (id) => {
+            let newTodoData = todoData.map((data) => {
                 if (data.id === id) {
-                // console.log('handleCompleteChange', id + ", "+ data.id);
-                data.completed = !data.completed;
+                    // console.log('handleCompleteChange', id + ", "+ data.id);
+                    data.completed = !data.completed;
                 }
                 return data;
             });
             setTodoData(newTodoData);
         }
 
-    return (
+        return (
             <div key={id} {...provided.draggableProps}
                 ref={provided.innerRef} {...provided.dragHandleProps}
                 className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex justify-between items-center w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
-                >
+            >
                 <div className="items-center">
-                <input type="checkbox" defaultChecked={false}
-                onChange={()=> handleCompleteChange(id)}/>
-                <span className={completed ? "line-through" : undefined}> {title}</span>
+                    <input type="checkbox" defaultChecked={false}
+                        onChange={() => handleCompleteChange(id)} />
+                    <span className={completed ? "line-through" : undefined}> {title}</span>
                 </div>
                 <div className="items-center">
-                <button className="px-4 py-2 float-right" onClick={() => handleClick(id)}>x</button>
+                    <button className="px-4 py-2 float-right" onClick={() => handleClick(id)}>x</button>
                 </div>
             </div>
-     )
-}
+        )
+    }
 
 export default List

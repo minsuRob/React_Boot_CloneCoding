dcd5ff4b37d095e59d94c5d440e35120d9675627
minsuLee <minsu.rob@gmail.com>
2022-03-08 11:33:58 +0900

    (Chapter 5-9) movieAPI에서 전달받은 video 링크를 iframe으로 재생하는 기능 추가
    styled-components를 이용하여 css 처리

--- (Chap5-6) react_netflix_clone/src/components/Banner.js
+++ (Chap5-6) react_netflix_clone/src/components/Banner.js
@@ -2,10 +2,13 @@ import axios from '../api/axios';
 import React, { useEffect, useState } from 'react'
 import requests from "../api/requests";
 import "./Banner.css"
+import styled from 'styled-components'
+
 
 export default function () {
   const [movie, setMovie] = useState(false);
-  useEffect(()=> {
+  const [isClicked, setIsClicked] = useState(false);
+  useEffect(() => {
     fetchData();
   }, [])
 
@@ -28,26 +31,85 @@ const truncate =(str, n) => {
   return str?.length > n ? str.substr(0, n-1) + "..." : str;
 }
 
-  return (
-    <header className="banner"
-            style={{backgroundImage:`url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
-                    backgroundPosition: "top center",
-                    backgroundSize: "cover"
+
+  // console.log("movie", movie);
+  // console.log("movie.video", movie.video);
+  // console.log("movie.videos", movie.videos);
+  if (!isClicked) {
+    return (
+      <header className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover"
         }}
-    >
-    <div className="banner__contents">
-        <h1 className="banner__title">
-            {movie.title || movie.name || movie.original_name}    
-        </h1>    
-        <div className='banner__buttons'>
-            <button className='banner__button play'>Play</button>
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie.title || movie.name || movie.original_name}
+          </h1>
+          <div className='banner__buttons'>
+            <button
+              className='banner__button play'
+              onClick={() => setIsClicked(true)}
+            >
+              Play
+            </button>
             <button className='banner__button info'>More Infomaition</button>
+          </div>
+
+          <h1 className='banner__description'>{truncate(movie?.overview, 100)}</h1>
         </div>
+        <div className='banner__fadeBottom' />
 
-        <h1 className='banner__description'>{truncate(movie?.overview, 100)}</h1>
-    </div>
-    <div className='banner__fadeBottom'/>
+      </header>
+    )
 
-    </header>
-  )
+  } else {
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe 
+          src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+          width="640"
+          height="360"
+          frameborder="0"
+          allow="autoplay; fullscreen"
+          ></Iframe>
+          clicked
+        </HomeContainer>
+      </Container>
+    );
+  }
+
 }
+
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after{
+    content:"";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`

983b8e26b5800ac4256ce76cc4c9ad98bb13423c
minsuLee <minsu.rob@gmail.com>
2022-03-08 12:41:15 +0900

    (Mission 2 -Basic Misson 1) 할일 목록 일괄 삭제 기능 추가
    할 일 목록 제목 옆에 새로운 버튼을 만들어서 그 버튼을 누를 시에 모든 할 일이 지워지는 기능을 만드세요. (함수 컴포넌트를 사용합니다.)

--- (Chap1-4) TodoList/src/App.js
+++ (Chap1-4) TodoList/src/App.js
@@ -37,11 +37,24 @@ export default function App(){
 
   }
 
+  const deleteAll=() => {
+
+    setTodoData([]);
+
+  }
+
     return(
       <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
         <div className="w-ful p-5 m-4 bg-white rounded shadow lg:w-3/4">
           <div className="flex justify-between mb-3">
-            <h1>할일</h1>
+            <div>
+              <h1 className="p-2 text-pink-500 ">할일</h1>
+            </div>
+            <div>
+              <button 
+              className="p-2 text-pink-400 border-2 border-pink-200 rounded hover:text-white hover:bg-blue-200"
+              onClick={() => deleteAll()}>rm -rf *</button>
+            </div>
           </div>
 
           <Lists todoData={todoData} setTodoData={setTodoData}/>

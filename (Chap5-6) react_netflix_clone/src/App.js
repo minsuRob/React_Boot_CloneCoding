cfa72924a3726a6b8ce14579af2bc3bd3a724c98
minsuLee <minsu.rob@gmail.com>
2022-03-13 17:06:54 +0900

    (Chpt 6-3) styled-components를 이용하여 footer 생성

--- (Chap5-6) react_netflix_clone/src/App.js
+++ (Chap5-6) react_netflix_clone/src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import Banner from './components/Banner';
 import Row from './components/Row';
 import requests from './api/requests';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
+      <Footer></Footer>
     </div>
   );
 }

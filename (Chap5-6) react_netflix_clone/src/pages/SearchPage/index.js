dfc131713750ef01a9e2cbbc8e78b5447246560b
minsuLee <minsu.rob@gmail.com>
2022-03-16 18:54:52 +0900

    (Chpt 6-10) search page 구현
    - 사용자 입력값으로 검색을 구현
    - 검색 값을 url에 처리하게 변경
    - useEffect에 입력값을 통해서 movie api 호출이후, 이미지 정보를 불러옴

--- (Chap5-6) react_netflix_clone/src/pages/SearchPage/index.js
+++ (Chap5-6) react_netflix_clone/src/pages/SearchPage/index.js
@@ -1,7 +1,64 @@
-import React from 'react'
+import axios from '../../api/axios';
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import './SearchPage.css';
 
 export default function SearchPage() {
-  return (
-    <div>MainPage</div>
-  )
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+  let qeury = useQuery();
+  const searchTerm = qeury.get("q");
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+
+      setSearchResults(request.data.results);
+
+    } catch (error) {
+      console.log("Error", error);
+    }
+
+    const renderSearchResults = () => {
+      return setSearchResults.length > 0 ? (
+        <section className='search-container'>
+          {searchResults.map((movie) => {
+            if (movie.backdrop_path !== null && movie.media_type !== "person") {
+              const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+              return (
+                <div className='movie'>
+                  <div className='movie__column-poster'>
+                    <img src={movieImageUrl} alt="" className='moive__poster' />
+                  </div>
+                </div>
+              );
+            }
+          })}
+        </section>
+
+      ) : (
+        <section className='no-results'>
+          <div className='no-results__text'>
+            <p>Your search for "{searchTerm}" did not have any matches.</p>
+            <p>Suggestion:</p>
+            <ul>
+              <li>Try different keywords</li>
+            </ul>
+          </div>
+        </section>
+      );
+    };
+
+    return renderSearchResults();
+
+  }
 }

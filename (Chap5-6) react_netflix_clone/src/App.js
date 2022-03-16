559712ae5c8a4950a408f4a57c9ec05fbaf9ae59
minsuLee <minsu.rob@gmail.com>
2022-03-16 19:39:06 +0900

    fix : search path 변경

--- (Chap5-6) react_netflix_clone/src/App.js
+++ (Chap5-6) react_netflix_clone/src/App.js
@@ -29,7 +29,7 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<MainPage />} />
           <Route path=':movieId' element={<DetailPage />} />
-          <Route path=':search' element={<SearchPage />} />
+          <Route path='search' element={<SearchPage />} />
         </Route>
       </Routes>
     </div>

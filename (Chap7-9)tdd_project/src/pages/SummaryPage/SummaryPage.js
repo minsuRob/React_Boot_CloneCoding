89f6d11989e62536eb591e46fd2ac0785476d212
minsuLee <minsu.rob@gmail.com>
2022-03-28 18:02:48 +0900

    (chpt8-3) 최초 summaryPage 추가

---
+++ (Chap7-9)tdd_project/src/pages/SummaryPage/SummaryPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function SummaryPage() {
+  return (
+    <div>
+        <form>
+            <input 
+            type="checkbox"
+            checked={false}
+            //onChange={(e) => setChecked(e.target.checked)}
+            id="confirm-checkbox"
+            /> {" "}
+            <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+            <br/>
+            <button disabled={true} type="submit">
+                주문 확인
+            </button>
+        </form>
+    </div>
+  )
+}
+
+export default SummaryPage

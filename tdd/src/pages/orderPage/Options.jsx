ec9cdd2b4b8d3b4de4b24440d91ca9b27d93017b
minsuLee <minsu.rob@gmail.com>
2022-03-30 19:18:38 +0900

    (Chpt 8-10) option 체크박스를 표현하는 html 정보 추가

---
+++ tdd/src/pages/orderPage/Options.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Options({ name }) {
+    return <form>
+        <input type="checkbox" id={`${name} option`} /> {" "}
+        <label htmlFor={`${name} option`}> {name} </label>
+    </form>
+}
+
+export default Options;

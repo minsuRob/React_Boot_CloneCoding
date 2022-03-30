2c53c6b0571ad72d034a9548f9e6074a7b63f90e
minsuLee <minsu.rob@gmail.com>
2022-03-30 19:17:27 +0900

    chore : 에러 페이지 추가

--- tdd/src/components/ErrorBanner.js
+++ tdd/src/components/ErrorBanner.js
@@ -1,16 +1,17 @@
 import React from "react";
 
 function ErrorBanner({ message }) {
-  let errorMessage = message || "에러입니다.";
+  let errorMsg = message || "에러입니다.";
 
   return (
-    <div
+      <div
       data-testid="error-banner"
-      style={{ backgroundColor: "red", color: "white" }}
-    >
-      {errorMessage}
-    </div>
-  );
+      style={{backgroundColor: "red", color: "white"}}
+      >
+          {errorMsg}
+      </div>
+  )
 }
-
 export default ErrorBanner;
+
+

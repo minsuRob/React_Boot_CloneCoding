ec53bad5f30ff237797ccb15641434737df4aa2d
minsuLee <minsu.rob@gmail.com>
2022-03-30 19:01:52 +0900

    fix : add useContext lib

--- tdd/src/pages/orderPage/Type.jsx
+++ tdd/src/pages/orderPage/Type.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import Products from './Products';
 
 export default function Type({orderType}) {

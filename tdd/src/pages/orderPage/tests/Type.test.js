f82c6f8b4a30047e62af6f8506cd9373338c8aba
minsuLee <minsu.rob@gmail.com>
2022-03-30 17:30:42 +0900

    (Chpt 8-6) 비동기로 이미지를 받아와서, 갯수와 이미지의 alt text를 체크하는 테스트 케이스 생성

---
+++ tdd/src/pages/orderPage/tests/Type.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import React, { useState } from 'react'
+
+test("dis img from server", async() => { // server에서 비동기처리해서 이미지 요청
+    render(<Type orderType="products" />);
+
+    const productImages = await screen.findAllByRole("img", { // file이 하나가 아니기에 findAllByRole 사용
+        name: /products$/i, // 정규식
+    });
+    expect(productImages).toHaveLength(2); // sample이 2개이기에
+
+    const altText = productImages.map((element) => element.alt);
+    expect(altText).toEqual(["America product", "England product"]);
+});

772084b1cf98aecbb000762b0b8dd912726b9a28
minsuLee <minsu.rob@gmail.com>
2022-04-01 15:59:32 +0900

    fix: modify Compile error

--- tdd/src/index.js
+++ tdd/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { OrderContext } from './contexts/OrderContext';
+import { OrderContext, OrderContextProvider } from './context/OrderContext';
 
 ReactDOM.render(
-    <OrderContext.Provider value={}>
+    <OrderContextProvider>
       <App />
-    </OrderContext.Provider>,
+    </OrderContextProvider>,
   document.getElementById('root')
 );
 

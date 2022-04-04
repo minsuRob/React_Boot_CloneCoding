6825e403511c957df015df6b4c3b4344c61d2340
minsuLee <minsu.rob@gmail.com>
2022-04-04 12:54:01 +0900

    (Chpt 9-8) 총금액 합산 테스트 케이스 생성

--- tdd/src/pages/orderPage/tests/calculate.test.js
+++ tdd/src/pages/orderPage/tests/calculate.test.js
@@ -3,6 +3,7 @@ import { render, screen } from "../../../test-utils";
 import userEvent from "@testing-library/user-event";
 import { OrderContextProvider } from "../../../context/OrderContext";
 import Type from "../Type"
+import OrderPage from "../OrderPage"
 
 test("update pd's total when pd change", async() => {
         render(<Type orderType="products"/>, {wrapper: OrderContextProvider});
@@ -50,3 +51,55 @@ test("update option's total when opt change", async() => {
     userEvent.click(dinnerCheckbox);
     expect(optionsTotal).toHaveTextContent('500');
 });
+
+
+describe("total price of goods and options", ()=>{
+    test("tota price start with 0 and Updating price when adding one product",async()=> {
+        render(<OrderPage/>);
+
+        const total = screen.getByText("총 금액:", {exact: false});
+        expect(total).toHaveTextContent("0");
+
+        const americaInput = await screen.findByRole("spinbutton", {
+            name: "America",
+        });
+        userEvent.clear(americaInput);
+        userEvent.type(americaInput, '1');
+
+        expect(total).toHaveTextContent("1000");
+    });
+
+    test("updating total price when adding one option",async()=> {
+        render(<OrderPage/>);
+        const total = screen.getByText("총 금액:", {exact: false});
+        const insuranceCheckbox = await screen.findByRole("checkbox", {// backend에서 가져온 후에 하기에 findByRole 사용
+            name: "Insurance",
+        });
+    
+        userEvent.click(insuranceCheckbox);
+        expect(total).toHaveTextContent('500');
+    });
+    test("updating total price when removing option and product",async()=> {
+        render(<OrderPage/>);
+        const total = screen.getByText("총 금액:", {exact: false});
+        
+        const insuranceCheckbox = await screen.findByRole("checkbox", {// backend에서 가져온 후에 하기에 findByRole 사용
+            name: "Insurance",
+        });
+    
+        userEvent.click(insuranceCheckbox);
+        
+        
+        const americaInput = await screen.findByRole("spinbutton", {
+            name: "America",
+        });
+        userEvent.clear(americaInput);
+        userEvent.type(americaInput, '3');
+
+        userEvent.clear(americaInput);
+        userEvent.type(americaInput, '1');
+
+        expect(total).toHaveTextContent("1500");
+
+    });
+})

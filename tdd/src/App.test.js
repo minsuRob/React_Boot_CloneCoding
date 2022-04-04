05f8de143fefc5442ad24aa4a8352873bb5ed7f0
minsuLee <minsu.rob@gmail.com>
2022-04-04 14:28:46 +0900

    (Chpt 9-10) 주문 확인 실제코드 및 테스트페이지 처리

--- tdd/src/App.test.js
+++ tdd/src/App.test.js
@@ -28,6 +28,27 @@ test('From order to ', async ()=>{
     });
     userEvent.click(orderButton);
 
-    //expect(optionsTotal).toHaveTextContent('500');
+    const summaryHeading = screen.getByRole("heading", {name: "주문 확인"});
+    expect(summaryHeading).toBeInTheDocument();
+
+    const productHeading = screen.getByRole("heading", {name: "여행 상품: 5000"});
+    expect(productHeading).toBeInTheDocument();
+
+    const optionHeading = screen.getByRole("heading", {name: "옵션: 500"});
+    expect(optionHeading).toBeInTheDocument();
+    
+    expect(screen.getByText("2 America")).toBeInTheDocument();
+    expect(screen.getByText("3 England")).toBeInTheDocument();
+    expect(screen.getByText("Insurance")).toBeInTheDocument();
+
+    const confirmCheckbox = screen.getByRole("checkbox", {
+        name: "주문하려는 것을 확인하셨나요?",
+    });
+    userEvent.click(confirmCheckbox);
+    
+    const confirmOrderButton = screen.getByRole("button", {
+        name: "주문 확인",
+    });
+    userEvent.click(confirmOrderButton);
 
 });
--- tdd/src/pages/SummaryPage/SummaryPage.js
+++ tdd/src/pages/SummaryPage/SummaryPage.js
@@ -1,24 +1,61 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { OrderContext } from '../../context/OrderContext'
 
-function SummaryPage() {
-  const [checked, setChecked] = useState(false)
-  
+function SummaryPage({setStep}) {
+  const [checked, setChecked] = useState(false);
+  const [orderDetails] = useContext(OrderContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setStep(2)
+  }
+
+  const productArray = Array.from(orderDetails.products);
+  const productList = productArray.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+
+  const hasOptions = orderDetails.options.size > 0;
+  let optionsDisplay = null;
+
+  if (hasOptions) {
+    const optionsArray = Array.from(orderDetails.options.keys());
+    const optionList = optionsArray.map((key) => <li key={key}>{key}</li>);
+
+    optionsDisplay = (
+      <div>
+        <h2>옵션: {orderDetails.totals.options}</h2>
+        <ul>{optionList}</ul>
+      </div>
+    );
+
+  }
+
+
   return (
     <div>
-        <form>
-            <input 
-            type="checkbox"
-            checked={checked}
-            //onChange={(e) => setChecked(e.target.checked)}
-            id="confirm-checkbox"
-            onChange={(e)=> setChecked(e.target.checked)}
-            /> {" "}
-            <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
-            <br/>
-            <button disabled={!checked} type="submit">
-                주문 확인
-            </button>
-        </form>
+      <h1>주문 확인</h1>
+      <h2>여행 상품: {orderDetails.totals.products}</h2>
+      <ul>{productList}</ul>
+
+      {optionsDisplay}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="checkbox"
+          checked={checked}
+          //onChange={(e) => setChecked(e.target.checked)}
+          id="confirm-checkbox"
+          onChange={(e) => setChecked(e.target.checked)}
+        /> {" "}
+        <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+        <br />
+        <button disabled={!checked} type="submit">
+          주문 확인
+        </button>
+      </form>
     </div>
   )
 }
